c0087d7f2c2a2c0651d852f06cc6a20f7ec584da
NBGamer99 <nabouziyasser80@gmail.com>
2022-06-24 14:13:28 +0100

    C06

--- C06/ex02/ft_rev_params.c
+++ C06/ex02/ft_rev_params.c
@@ -3,11 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   ft_rev_params.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ynabouzi <ynabouzi@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: ynabouzi <ynabouzi@student.1337.ma >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 23:37:47 by ynabouzi          #+#    #+#             */
-/*   Updated: 2022/06/23 23:51:56 by ynabouzi         ###   ########.fr       */
+/*   Updated: 2022/06/24 13:08:44 by ynabouzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+	char *str;
+
+	// str = NULL;
+	j = 0;
+	i = ac - 1;
+	if (ac >= 2)
+	{
+		while (i >= 1)
+		{
+			str = &av[i][0];
+			j = 0;
+			while (str[j] != '\0')
+			{
+				ft_putchar(str[j]);
+				j++;
+			}
+			write(1, "\n", 1);
+			i--;
+		}
+	}
+}
