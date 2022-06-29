ae1e6156c77a69e7ac1cd1d6cffc21c524a87567
NBGamer99 <nabouziyasser80@gmail.com>
2022-06-29 04:16:48 +0100

    hello

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -1,7 +1,16 @@
+# ifndef FT_BOOLEAN_H
+
+# define FT_BOOLEAN_H
+
+# define SUCCESS 0
+#include <unistd.h>
+typedef int t_bool;
+# define EVEN(var) (var % 2)
+
 # define FALSE 0
 # define TRUE (!FALSE)
 
-# define ODD_MSG "I HAVE AN ODD "
-# define SUCCESS 0
-
+# define ODD_MSG "I HAVE AN ODD NUMBER"
+# define EVEN_MSG "I HAVE AN EVEN NUMBER"
+# endif
 
---
+++ C08/ex02/main.c
@@ -0,0 +1,8 @@
+#include "ft_abs.h"
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("%d",ABS(-10));
+}
